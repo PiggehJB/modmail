0a8e71d168d78bfdb836894cc72bd9ff55860394
PiggehJB <90732485+PiggehJB@users.noreply.github.com>
2021-12-12 19:40:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ import os
 from datetime import datetime
 from discord.colour import Color
 from replit import db
-
-
-# Make sure you have keep_alive.py created before importing this!
 from keep_alive import keep_alive
 
 def delete_keys():
@@ -40,8 +37,7 @@ bot = commands.Bot(
 @bot.event
 async def on_ready():
   print(f"{bot.user.name} <- Online")
-  # Deletes the keys (users in database, remove upon hosting bot).
-  delete_keys()
+  
 
 @bot.event
 async def on_message(ctx):
@@ -58,7 +54,32 @@ async def on_message(ctx):
   if not ctx.guild:
     # Returning user
     if find_key(ctx.author.id) is True:
-      channel = bot.get_channel(db[str(ctx.author.id)])
+      try:
+        channel = bot.get_channel(db[str(ctx.author.id)])
+
+        embed = discord.Embed(
+          title="Message From [{}]".format(ctx.author.name),
+          color = Color.random(),
+          timestamp = datetime.utcnow(),
+          description = ctx.content
+        )
+        # Bottom Text
+        embed.set_footer(
+          text=f"Sent by {ctx.author} • {ctx.author.id}",
+          icon_url = ctx.author.avatar_url
+        )
+
+        # Top Text
+        embed.set_author(
+          name=ctx.author.name,
+          icon_url = ctx.author.avatar_url
+        )
+
+        await channel.send(embed=embed)
+        await ctx.add_reaction(emoji='<a:check_emoji:909260788802408448>')
+      except Exception as e:
+        print(e)
+        await ctx.add_reaction(emoji='❌')
 
     else:
       try:
@@ -84,5 +105,49 @@ async def on_message(ctx):
       except:
         await ctx.add_reaction(emoji='❌')
       
+  elif find_key(ctx.channel.id) is True:
+    try:
+      user = discord.utils.get(guild.members, id=db[str(ctx.channel.id)])
+      if ctx.content == '.close':
+        close_embed = discord.Embed(
+          title=f"Goodbye [{user.name}]",
+          color = Color.blue(),
+          description=f"Goodbye {user.mention}!\nWe are now closing this ticket, if you need anything else feel free to send us another message!",
+          timestamp = datetime.utcnow()
+        )
+        close_embed.set_footer(text="By replying you are opening another ticket.")
+        await user.send(embed=close_embed)
+        channel = bot.get_channel(db[str(user.id)])
+        await channel.delete()
+        del db[str(ctx.channel.id)]
+        del db[str(user.id)]
+        return 
+      
+      else:
+        embed = discord.Embed(
+          title="From {}".format(ctx.guild.name),
+          color = Color.random(),
+          timestamp = datetime.utcnow(),
+          description = ctx.content
+        )
 
+        embed.set_footer(
+          text=f"Sent by {ctx.author} • {ctx.author.id}",
+          icon_url = ctx.author.avatar_url
+        )
+
+        # Top Text
+        embed.set_author(
+          name=ctx.author.name,
+          icon_url = ctx.author.avatar_url
+        )
+        await user.send(embed=embed)
+
+        await ctx.add_reaction(emoji='✅')
+    except Exception as e:
+      print(e)
+      await ctx.add_reaction(emoji='❌')
+
+
+keep_alive()
 bot.run(token)
